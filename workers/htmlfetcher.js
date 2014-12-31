0ab6e9fee8e0451f351bf80ab93a8cdd61cc2395
Valentyn Boginskey <poison5151@gmail.com>
2014-12-31 09:35:30 -0800

    Removed extraneous comments

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,5 +1,3 @@
-// eventually, you'll have some code here that uses the code in `archive-helpers.js`
-// to actually download the urls you want to download.
 var archive = require('../helpers/archive-helpers');
 var request = require('http-request');
 
